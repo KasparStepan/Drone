baf403fb4a95e1f9c91ea145d8abacb16714b2c9
KasparStepan <xkaspa43@vutbr.cz>
2024-04-14 09:55:50 +0200

    fuselage

--- test2.py/test2.py
+++ test2.py/test2.py
@@ -65,77 +65,35 @@ airplane = asb.Airplane(
             symmetric=True,
             xsecs=[
                 asb.WingXSec(
-                    xyz_le=[0, 0, 0],
+                    xyz_le=[0, 0.2, 0],
                     chord=0.1,
                     twist=0,
                     airfoil=asb.Airfoil("naca0012"),
                     
                 ),
                 asb.WingXSec(
-                    xyz_le=[0, 0.2, 0.15],
+                    xyz_le=[0.04, 0, 0.15],
                     chord=0.06,
                     twist=0,
                     airfoil=asb.Airfoil("naca0012")
                 )
             ]
-        ).translate([0.6, 0, 0])    
+        ).translate([0.6, 0, 0.07])    
     ],
 
-    fuselages = [asb.Fuselage(
-        name="Fuselage tip",
-        xsecs=[
-            asb.FuselageXSec(
-                xyz_c = [ xi*0.1,0,0],
-                # radius = np.sin(xi/0.1*np.pi/2)*0.05
-                radius = np.sqrt(1-(-1+xi)**2)*0.025
-            )for xi in np.cosspace(0,1,30)            
-
-
-        ]
-    ),
-    asb.Fuselage(
-        name="Fuselage body",
-        xsecs=[
-            asb.FuselageXSec(
-                xyz_c = [0.1,0,0],
-                radius = 0.025               
+    fuselages = [
+        asb.Fuselage(
+            name="Fuselage",
+            xsecs=[
+                asb.FuselageXSec(
+                    xyz_c = [ 0,0,0],
+                    radius = 0,
             ),
-            asb.FuselageXSec(
-                xyz_c = [0.25,0,0],
-                radius = 0.025
+                asb.FuselageXSec(
+                    xyz_c=[0.03,0,0],
+                    radius = 0.2,
             )
         ]
-    ),
-    asb.Fuselage(
-        name ="Fuselage trannsition",
-        xsecs = [
-            asb.FuselageXSec(
-                xyz_c=[0.25+xi/np.pi*0.1,0,0],
-                radius = (np.cos(xi)+1)*0.0125/2+0.0125
-            )for xi in np.cosspace(0,np.pi,30)
-        ]
-    ),
-    asb.Fuselage(
-        name = "Fuselage Tail",
-        xsecs=[
-            asb.FuselageXSec(
-                xyz_c = [0.35,0,0],
-                radius = 0.0125               
-            ),
-            asb.FuselageXSec(
-                xyz_c = [0.7,0,0],
-                radius = 0.0125
-            )
-        ]
-    ),
-    asb.Fuselage(
-        name="Fuselage Tail tip ",
-        xsecs=[
-            asb.FuselageXSec(
-                xyz_c = [ 0.7+xi*0.05,0,0],
-                radius = np.sqrt(1-(xi)**2)*0.0125
-            )for xi in np.cosspace(0,1,30)
-    ]
     )
     ]
 )
