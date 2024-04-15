57e54caf39080c78f02acf189f72f3adc0e3d907
KasparStepan <xkaspa43@vutbr.cz>
2024-04-15 23:27:25 +0200

    First iteration of weight estimation and geometry definition

--- drone_opti.py
+++ drone_opti.py
@@ -1,34 +1,63 @@
 import aerosandbox as asb
 import aerosandbox.numpy as np
 from aerosandbox.library import airfoils
+import copy
+
+# Enviroment Values
+g = 9.80665
 
 
 
-#Fluid properties
-density = 1.225
-viscosity = 1.81e-5
-xyz_ref = [0,0,0]
+opti = asb.Opti(
+    freeze_style = 'float'
+)
+
+infill_default = 0.2
+skin_thickness_default = 0.0015
+
+filament_density = {
+          "PLA": 1250,
+          "LW-PLA": 800
+}
+
+
+skin_thickness = {
+          "Center wing": skin_thickness_default,
+          "Tip wing": skin_thickness_default,
+          "Tail wing": skin_thickness_default,
+          "Fuselage tip" : skin_thickness_default,
+          "Fuselage body": skin_thickness_default,
+          "Fuselage transition": skin_thickness_default,
+          "Fuselage tail": skin_thickness_default,
+          "Fuselage tail tip": skin_thickness_default,
+}
+
+infill = {
+          "Center wing": infill_default,
+          "Tip wing": infill_default,
+          "Tail wing": infill_default,
+          "Fuselage tip" : infill_default,
+          "Fuselage body": infill_default,
+          "Fuselage transition": infill_default,
+          "Fuselage tail": infill_default,
+          "Fuselage tail tip": infill_default,
+}
+
 
     
     
 op_point = asb.OperatingPoint(
     atmosphere=asb.Atmosphere(altitude=0),
-    velocity=91.3,  # m/s
+    velocity=25,  # m/s
     )
 
 
-airplane = asb.Airplane(
-    name = "SKD",
-    xyz_ref = [0,0,0],
-    s_ref = 9, #Reference area
-    c_ref = 0.9, #Reference chord
-    b_ref = 10, #Reference span
-    
+airfoil_main_wing = asb.Airfoil("rg15")
 
 
-    wings=[
-        asb.Wing(
-            name = "Main wing",
+## Definition of Main wing
+Center_wing = asb.Wing(
+            name = "Center wing",
             xyz_le=[0, 0, 0],
             symmetric=True,
             xsecs=[# The wing's cross ("X") sections, or "XSecs"
@@ -36,27 +65,38 @@ airplane = asb.Airplane(
                     xyz_le = [0.1,0,0],
                     chord = 0.15, #meters
                     twist = 0, #degrees
-                    airfoil=asb.Airfoil("s1223"), # Flap # Control surfaces are applied between a given XSec and the next one.
-                    
+                    airfoil=airfoil_main_wing, # Flap # Control surfaces are applied between a given XSec and the next one.
                     ),
                 asb.WingXSec(#Mid
                     xyz_le = [0.1,0.5,0],
                     chord = 0.13,
                     twist = -2,
-                    airfoil=asb.Airfoil("s1223"),
-                    
-                    
-                    
+                    airfoil=airfoil_main_wing,
+                )
+                ]
+            )
+
+Tip_wing = asb.Wing(
+            name = "Tip wing",
+            xyz_le=[0, 0, 0],
+            symmetric=True,
+            xsecs=[# The wing's cross ("X") sections, or "XSecs"
+                    asb.WingXSec(#Mid
+                    xyz_le = [0.1,0.5,0],
+                    chord = 0.13,
+                    twist = -2,
+                    airfoil=airfoil_main_wing,
                 ),
                 asb.WingXSec(#Tip
                     xyz_le = [0.1,0.8,0.12],
                     chord = 0.05,
                     twist = -10,
-                    airfoil=asb.Airfoil("s1223"),
+                    airfoil=airfoil_main_wing,
                     ),
                 ]
-        ),
-        asb.Wing(
+            )
+
+Tail_wing = asb.Wing(
             name="Vertical Stabilizer",
             symmetric=True,
             xsecs=[
@@ -74,10 +114,9 @@ airplane = asb.Airplane(
                     airfoil=asb.Airfoil("naca0012")
                 )
             ]
-        ).translate([0.6, 0, 0])    
-    ],
+            ).translate([0.6, 0, 0])
 
-    fuselages = [asb.Fuselage(
+fuselage_tip = asb.Fuselage(
         name="Fuselage tip",
         xsecs=[
             asb.FuselageXSec(
@@ -88,8 +127,9 @@ airplane = asb.Airplane(
 
 
         ]
-    ),
-    asb.Fuselage(
+        )
+
+fuselage_body = asb.Fuselage(
         name="Fuselage body",
         xsecs=[
             asb.FuselageXSec(
@@ -101,18 +141,19 @@ airplane = asb.Airplane(
                 radius = 0.025
             )
         ]
-    ),
-    asb.Fuselage(
-        name ="Fuselage trannsition",
+    )
+fuselage_transition = asb.Fuselage(
+        name ="Fuselage transition",
         xsecs = [
             asb.FuselageXSec(
                 xyz_c=[0.25+xi/np.pi*0.1,0,0],
                 radius = (np.cos(xi)+1)*0.0125/2+0.0125
             )for xi in np.cosspace(0,np.pi,30)
         ]
-    ),
-    asb.Fuselage(
-        name = "Fuselage Tail",
+    )
+
+fuselage_tail = asb.Fuselage(
+        name = "Fuselage tail",
         xsecs=[
             asb.FuselageXSec(
                 xyz_c = [0.35,0,0],
@@ -123,9 +164,10 @@ airplane = asb.Airplane(
                 radius = 0.0125
             )
         ]
-    ),
-    asb.Fuselage(
-        name="Fuselage Tail tip ",
+    )
+
+fuselage_tail_tip = asb.Fuselage(
+        name="Fuselage tail tip",
         xsecs=[
             asb.FuselageXSec(
                 xyz_c = [ 0.7+xi*0.05,0,0],
@@ -133,8 +175,124 @@ airplane = asb.Airplane(
             )for xi in np.cosspace(0,1,30)
     ]
     )
+
+airplane = asb.Airplane(
+    name = "SKD",
+    xyz_ref = [0,0,0],
+    s_ref = 9, #Reference area
+    c_ref = 0.9, #Reference chord
+    b_ref = 10, #Reference span
+    wings=[
+        Center_wing,
+        Tip_wing,        
+        Tail_wing    
+    ],
+
+    fuselages = [
+        fuselage_tip,
+        fuselage_body,
+        fuselage_transition,
+        fuselage_tail,
+        fuselage_tail_tip
     ]
 )
+
+
+
+mass_props = {}
+
+## Lifting bodies
+
+mass_props['Center_wing'] = asb.mass_properties_from_radius_of_gyration(
+          mass = (Center_wing.volume()*infill["Center wing"]+Center_wing.area("wetted")*skin_thickness["Center wing"])*filament_density["PLA"],
+          x_cg = 0,
+          y_cg = 0,
+          z_cg = 0,
+          radius_of_gyration_x = 0,
+          radius_of_gyration_y = 0,
+          radius_of_gyration_z = 0,
+
+)
+
+mass_props['Tip_wing'] = asb.mass_properties_from_radius_of_gyration(
+          mass = (Tip_wing.volume()*infill["Tip wing"]+Tip_wing.area("wetted")*skin_thickness["Tip wing"])*filament_density["PLA"],
+          x_cg = 0,
+          y_cg = 0,
+          z_cg = 0,
+          radius_of_gyration_x = 0,
+          radius_of_gyration_y = 0,
+          radius_of_gyration_z = 0,
+
+)
+
+mass_props['Tail_wing'] = asb.mass_properties_from_radius_of_gyration(
+          mass = (Tail_wing.volume()*infill["Tail wing"]+Tail_wing.area("wetted")*skin_thickness["Tail wing"])*filament_density["PLA"],
+          x_cg = 0,
+          y_cg = 0,
+          z_cg = 0,
+          radius_of_gyration_x = 0,
+          radius_of_gyration_y = 0,
+          radius_of_gyration_z = 0,
+
+)
+
+for i in airplane.fuselages:
+          mass_props[i.name] = asb.mass_properties_from_radius_of_gyration(
+                    (i.volume()*infill[i.name]+i.area_wetted()*skin_thickness[i.name])*filament_density["PLA"],
+                    x_cg = i.x_centroid_projected('XY')
+          )
+
+mass_props["Electric motor"] = asb.mass_properties_from_radius_of_gyration(
+          mass = 0.05, # Needs to be researched
+          x_cg = 0,
+          y_cg = 0,
+          z_cg = 0,
+          radius_of_gyration_x = 0,
+          radius_of_gyration_y = 0,
+          radius_of_gyration_z = 0,
+)
+
+mass_props["Propeller"] = asb.mass_properties_from_radius_of_gyration(
+          mass = 0.04, # Needs to be researched
+          x_cg = 0,
+          y_cg = 0,
+          z_cg = 0,
+          radius_of_gyration_x = 0,
+          radius_of_gyration_y = 0,
+          radius_of_gyration_z = 0,
+)
+
+mass_props['Motor mount'] = copy.copy(
+    mass_props['Electric motor']
+) * 1
+
+mass_props['Avionics'] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.060,  # RX, pixhawk mini
+    x_cg=0,
+    z_cg=0
+)
+
+mass_props['servos'] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.050, #find the number and the weight
+    x_cg=0
+)
+
+mass_props['battery'] = asb.mass_properties_from_radius_of_gyration(
+    mass=0, #To be determined
+    x_cg=0,
+    z_cg=0
+)
+
+mass_props["Payload"] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.25, #To be determined
+    x_cg=0,
+    z_cg=0
+)
+
+print(sum(mass_props.values()))
+
+
+Lift_required = sum(mass_props.values())*g
 
 ## Aero builduup
 
@@ -152,50 +310,50 @@ airplane = asb.Airplane(
 
 ## Nonlinear lifting Line 
 
-nlll_op_point = op_point.copy()
-nlll_op_point.alpha = np.linspace(-10, 10, 5)
+# nlll_op_point = op_point.copy()
+# nlll_op_point.alpha = np.linspace(-10, 10, 5)
 
-nlll_aeros = [
-    asb.NonlinearLiftingLine(
-        airplane=airplane,
-        op_point=op,
-        xyz_ref=xyz_ref,
-    ).run()
-    for op in nlll_op_point
-]
+# nlll_aeros = [
+#     asb.NonlinearLiftingLine(
+#         airplane=airplane,
+#         op_point=op,
+#         xyz_ref=xyz_ref,
+#     ).run()
+#     for op in nlll_op_point
+# ]
 
-nlll_aero = {}
-for k in nlll_aeros[0].keys():
-    nlll_aero[k] = np.array([
-        aero[k]
-        for aero in nlll_aeros
-    ])
-nlll_aero["alpha"] = nlll_op_point.alpha
+# nlll_aero = {}
+# for k in nlll_aeros[0].keys():
+#     nlll_aero[k] = np.array([
+#         aero[k]
+#         for aero in nlll_aeros
+#     ])
+# nlll_aero["alpha"] = nlll_op_point.alpha
 
-print(nlll_aero)
+# print(nlll_aero)
 
-## Quasi Lifting line
-ll_op_point = op_point.copy()
-ll_op_point.alpha = np.linspace(0, 14, 4)
+# ## Quasi Lifting line
+# ll_op_point = op_point.copy()
+# ll_op_point.alpha = np.linspace(0, 14, 4)
 
-ll_aeros = [
-    asb.LiftingLine(
-        airplane=airplane,
-        op_point=op,
-        xyz_ref=xyz_ref,
-    ).run()
-    for op in ll_op_point
-]
+# ll_aeros = [
+#     asb.LiftingLine(
+#         airplane=airplane,
+#         op_point=op,
+#         xyz_ref=xyz_ref,
+#     ).run()
+#     for op in ll_op_point
+# ]
 
-ll_aero = {}
-for k in ll_aeros[0].keys():
-    ll_aero[k] = np.array([
-        aero[k]
-        for aero in ll_aeros
-    ])
-ll_aero["alpha"] = ll_op_point.alpha
+# ll_aero = {}
+# for k in ll_aeros[0].keys():
+#     ll_aero[k] = np.array([
+#         aero[k]
+#         for aero in ll_aeros
+#     ])
+# ll_aero["alpha"] = ll_op_point.alpha
 
-print(ll_aero)
+# print(ll_aero)
 
 
 
--- initial_sizing.py
+++ initial_sizing.py
@@ -20,11 +20,247 @@ cruise_op_point = asb.OperatingPoint(
 )
 
 ### Definition of parameters to optimize
-AR = opti.variable() #Aspect ratio [-]
-S = opti.variable() # Wing Area [-]
+Aspect_ratio = opti.variable() #Aspect ratio [-]
+wing_area = opti.variable() # Wing Area [-]
+taper_ratio = opti.variable() #Taper ratio [-]
+V_ht = opti.variable() # Doplnit
+V_vt = opti.variable() # Doplnit
+Aspect_ratio_ht = opti.variable() 
+Aspect_ratio_vt = opti.variable()
 
 
 
-# Flow parameters
 
+# Enviroment parameters
+g = 9.81
+viscosity = 1.775e-5
+density = 1.225
 altitude = 0 # m ISA
+
+# Base airplane parameters
+
+weight_payload = 3
+endurance = 1
+fuselage_radius = 0.025 # poloměr draku [m]
+design_weight = 2 # Očekávaná hmotnost
+
+
+## Aerodynamics design 
+chord_ref = wing_area
+b_ref = Aspect_ratio/chord_ref
+
+
+#Návrh ocasních plochy
+
+chord_root = 2*chord_ref/(1+taper_ratio)
+chord_tip = chord_root*taper_ratio
+
+chord_mgc = 2/3*chord_root*(1+taper_ratio*taper_ratio**2)/(1+taper_ratio)
+
+l_t = np.sqrt((2*V_ht*wing_area*chord_mgc)/np.pi*(2*fuselage_radius))
+
+S_ht = V_ht*wing_area*chord_ref/l_t
+b_ht = np.sqrt(Aspect_ratio_ht*S_ht)
+chord_ht_ref = b_ht/Aspect_ratio_ht
+
+S_vt = V_vt*wing_area*b_ref/l_t
+b_vt = np.sqrt(Aspect_ratio_vt*S_vt)
+chord_ht_ref = b_vt/Aspect_ratio_vt
+
+
+
+### Weight estimation
+##### Section: Internal Geometry and Weights
+
+structural_mass_markup = 1.2  # over Enigma F5J
+
+mass_props = {}
+
+### Lifting bodies
+mass_wing = (  # Engima F5J
+                    (0.440 + 0.460) *
+                    (wing_area() / (0.264 * 3.624 * np.pi / 4)) ** 0.758 *
+                    (design_weight / 1.475) ** 0.49 *
+                    (Aspect_ratio() / 18) ** 0.6
+            ) * structural_mass_markup
+
+mass_props['wing_center'] = asb.mass_properties_from_radius_of_gyration(
+    mass=mass_wing * wing_y_break_fraction,
+    x_cg=0,  # quarter-chord,
+    radius_of_gyration_x=(wing_y_break_fraction * wing_span) / 12,
+    radius_of_gyration_z=(wing_y_break_fraction * wing_span) / 12
+)
+mass_props['wing_tips'] = asb.mass_properties_from_radius_of_gyration(
+    mass=mass_wing * (1 - wing_y_break_fraction),
+    x_cg=0,  # quarter-chord,
+    radius_of_gyration_x=(1 + wing_y_break_fraction) / 2 * (wing_span / 2),
+    radius_of_gyration_z=(1 + wing_y_break_fraction) / 2 * (wing_span / 2),
+)
+mass_props['h_stab'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(
+                 0.055 *
+                 (
+                         0.4 * (design_mass_TOGW / 1.475) ** 0.40 * (hstab_span / (0.670 / np.cosd(40))) ** 1.58 +
+                         0.6 * (wing_root_chord / 0.264) * (hstab_span / (0.670 / np.cosd(40)))
+                 )
+         ) * structural_mass_markup,
+    x_cg=hstab.xsecs[0].xyz_le[0] + hstab_chord / 2,
+    z_cg=vstab.aerodynamic_center()[2],
+    radius_of_gyration_x=hstab_span / 12,
+    radius_of_gyration_y=hstab.xsecs[0].xyz_le[0] + hstab_chord / 2,
+    radius_of_gyration_z=hstab.xsecs[0].xyz_le[0] + hstab_chord / 2
+)
+mass_props['v_stab'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(
+                 0.055 *
+                 (
+                         0.3 * (design_mass_TOGW / 1.475) ** 0.40 * (vstab_span / (0.670 / np.cosd(40))) ** 1.58 +
+                         0.7 * (wing_root_chord / 0.264) * (vstab_span / (0.670 / np.cosd(40)))
+                 )
+         ) * structural_mass_markup,
+    x_cg=vstab.xsecs[0].xyz_le[0] + vstab_chord / 2,
+    z_cg=vstab.aerodynamic_center()[2],
+    radius_of_gyration_x=vstab_span / 12,
+    radius_of_gyration_y=vstab.xsecs[0].xyz_le[0] + vstab_chord / 2,
+    radius_of_gyration_z=vstab.xsecs[0].xyz_le[0] + vstab_chord / 2
+)
+
+### Other Structure
+mass_props['boom'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(
+                 0.235 *
+                 (x_tail / 1.675) *
+                 (design_mass_TOGW / 1.475) ** 0.49
+         ) * structural_mass_markup,
+    x_cg=x_tail / 2,
+    radius_of_gyration_y=x_tail / 3,
+    radius_of_gyration_z=x_tail / 3,
+)
+
+### Propulsion
+mass_props['motors'] = asb.mass_properties_from_radius_of_gyration(
+    mass=lib_prop_elec.mass_motor_electric(
+        max_power=climb_power_propulsion / n_propellers,
+        kv_rpm_volt=motor_kv,
+        voltage=battery_voltage,
+    ) * n_propellers,
+    x_cg=-0.25 * wing_root_chord - 2 * u.inch,
+    radius_of_gyration_x=motor_y_placement,
+    radius_of_gyration_y=0.25 * wing_root_chord + 2 * u.inch,
+    radius_of_gyration_z=motor_y_placement,
+)
+
+mass_props['motor_mounts'] = copy.copy(
+    mass_props['motors']
+) * 1  # similar to a quote from Raymer, modified to make sensible units, prop weight roughly subtracted
+
+mass_props['propellers'] = asb.mass_properties_from_radius_of_gyration(
+    mass=n_propellers * lib_prop_prop.mass_hpa_propeller(
+        diameter=propeller_diameter,
+        max_power=climb_power_propulsion / n_propellers,
+        include_variable_pitch_mechanism=False
+    ),
+    x_cg=mass_props['motors'].x_cg - 1 * u.inch,
+    radius_of_gyration_x=motor_y_placement,
+    radius_of_gyration_z=motor_y_placement,
+)
+
+mass_props['ESCs'] = asb.mass_properties_from_radius_of_gyration(
+    mass=lib_prop_elec.mass_ESC(max_power=climb_power_propulsion / n_propellers) * n_propellers,
+    x_cg=0,
+)
+
+### Fuselage internals
+mass_props['fuselage_skin'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(
+                 fuse_wetted_area * (
+                 np.minimum(2, 2 * fuselage_length) *  # plies
+                 (4 * u.oz / u.yard ** 2)
+         )
+         ) * 1.5  # waterproofing markup
+    ,
+    x_cg=0.4 + x_fuse_nose,  # 0.4 to weight more towards thicker nose
+    z_cg=-0.15 * fuselage_length
+)
+mass_props['fuselage_bulkheads'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(
+            fuse_xsec_area * (  # These won't be anywhere near solid, but this is a rough estimate of material needed
+            4 *  # n_bulkheads
+            (1 / 8 * u.inch) * fuselage_length * (400)  # basswood
+    )
+    ),
+    x_cg=(0.35 + x_fuse_nose) / 2,  # halfway between mid-fuse location and wing quarter-chord.
+    z_cg=-0.15 * fuselage_length
+)
+
+mass_props['avionics'] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.060,  # RX, pixhawk mini
+    x_cg=5 * u.inch + x_fuse_nose,
+    z_cg=-0.15 * fuselage_length
+)
+
+mass_props['servos'] = asb.mass_properties_from_radius_of_gyration(
+    mass=0.050,
+    x_cg=x_tail * 0.5  # 2x tail, 2x wing
+)
+
+battery_capacity = (
+        battery_reserve_time * (3 / 4) * cruise_power_propulsion +
+        battery_reserve_time * (1 / 4) * climb_power_propulsion
+)
+
+battery_ampacity_amp_hours = battery_capacity / u.hour / battery_voltage
+
+mass_props['battery'] = asb.mass_properties_from_radius_of_gyration(
+    mass=battery_capacity / u.hour / battery_specific_energy_Wh_kg,
+    x_cg=x_fuse_nose + 3 * u.inch,
+    z_cg=-0.15 * fuselage_length
+)
+
+mass_props['MPPTs'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(n_panels / 36) * 0.080,  # Genasun GV-5
+    x_cg=x_fuse_nose + 5 * u.inch,
+    z_cg=-0.15 * fuselage_length
+)
+
+mass_props['solar_cells'] = asb.mass_properties_from_radius_of_gyration(
+    mass=solar_area * rho_solar_cells,
+    x_cg=0.25 * wing_root_chord,  # at the half-chord (remembering that datum is at quarter-chord)
+)
+
+mass_props['wiring'] = asb.mass_properties_from_radius_of_gyration(
+    mass=(n_panels / 72) * 0.100,
+    x_cg=-wing_root_chord / 4,  # at the leading edge
+    z_cg=-0.05 * fuselage_length
+)
+
+### Other
+mass_props['wing_sponsons'] = asb.mass_properties_from_radius_of_gyration(
+    mass=2 * (  # 2 sponsons
+            wing_sponson_wetted_area * (
+            2 *  # plies
+            (4 * u.oz / u.yard ** 2)
+    )
+    ) * 1.5  # waterproofing markup
+    ,
+    x_cg=0  # quarter-chord
+)
+mass_props['wing_sponson_mounts'] = copy.copy(
+    mass_props['wing_sponsons']
+) * 2  # a total guess
+
+### Summation
+mass_props_TOGW = sum(mass_props.values())
+
+### Add glue weight
+mass_props['glue_weight'] = mass_props_TOGW * 0.08
+mass_props_TOGW += mass_props['glue_weight']
+
+
+
+
+
+
+
+
+
